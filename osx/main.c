2eff7afb29dcf64df4f7bc0801fa8ec5ead1ba09
Oleksandr <35868682+SanSanTVssi@users.noreply.github.com>
2022-05-24 01:31:27 +0300

    add document

--- osx/main.c
+++ osx/main.c
@@ -7,16 +7,18 @@
 #include "barrier.h"
 #include "functions.h"
 
-#define InitArgs \
-pair boundaries; \
-pair_set_values(A, B, boundaries); \
-pair otherArgs; \
-pair_set_values(N, H, otherArgs); \
-pair args; \
-pair_set_values(boundaries, otherArgs, args);
+#define InitArgs(args) \
+{ \
+    pair boundaries; \
+    pair_set_values(A, B, boundaries); \
+    pair otherArgs; \
+    pair_set_values(N, H, otherArgs); \
+    pair_set_values(boundaries, otherArgs, args); \
+}
 
 int main() {
-    InitArgs
+    pair args;
+    InitArgs(args);
 
     pthread_t pthread1;
     pthread_t pthread2;
@@ -28,15 +30,15 @@ int main() {
     pthread_create(&pthread3, NULL,  &function3, &args);
     
     printf("| index %4|\t\tx\t\t|\tcos^2(x)+sin(x)\t| sin^2(x)*(1+cos(x))\t|sin(x)*(1+cos^2(x))|\n %s",
-           "_____________________________________________________________________________\n");
+           "___________________________________________________________________________________________\n");
     for (int i = 0; i <= N; i++) {
         float x = CalculateX(A, H, i);
         usleep(5000);
         printf("|%8d\t|", i);
         printf("%16.6f\t|", x);
-        printf("%24.6f\t|", GetFunction1Result());
-        printf("%24.6f\t|", GetFunction2Result());
-        printf("%24.6f\t|", GetFunction3Result());
+        printf("%16.6f\t|", GetFunction1Result());
+        printf("%16.6f\t|", GetFunction2Result());
+        printf("%16.6f\t|", GetFunction3Result());
         printf("\n");
         pthread_barrier_wait(GetBarrierPointer());
     }
